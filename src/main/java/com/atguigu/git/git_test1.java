429155831db2a211b7d928052eb10ed98d5fb9f7
root <root@qq.com>
2022-05-03 16:37:28 +0800

    master branch_1_test commit

--- src/main/java/com/atguigu/git/git_test1.java
+++ src/main/java/com/atguigu/git/git_test1.java
@@ -8,6 +8,7 @@ public class git_test1 {
         System.out.println("git-test3");
         System.out.println("git-test4");
         System.out.println("git-test5");
+        System.out.println("git-branch_1_test");
     }
 
 
