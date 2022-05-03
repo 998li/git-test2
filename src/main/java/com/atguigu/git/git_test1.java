d30ab3570c2ab0d9140ffac7cf9fbf1c18e3ce48
root <root@qq.com>
2022-05-03 16:17:45 +0800

    branche_1 first commit

--- src/main/java/com/atguigu/git/git_test1.java
+++ src/main/java/com/atguigu/git/git_test1.java
@@ -4,6 +4,7 @@ public class git_test1 {
     public static void main(String[] args) {
         System.out.println("git-test");
         System.out.println("git-test1");
+        System.out.println("git-test2");
     }
 
 
