b2f2b5099461d6f5ba236a5dc660f212ec5c5c52
root <root@qq.com>
2022-05-03 16:10:39 +0800

    second commit

--- src/main/java/com/atguigu/git/git_test1.java
+++ src/main/java/com/atguigu/git/git_test1.java
@@ -3,8 +3,7 @@ package com.atguigu.git;
 public class git_test1 {
     public static void main(String[] args) {
         System.out.println("git-test");
-
-
+        System.out.println("git-test1");
     }
 
 
